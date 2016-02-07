cc229db815986a72871c9f3c313dd303f6000896
Amar Zeno <Amar Zeno>
2016-02-08 02:05:09 +0530

    score system added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
     public float speed = 8;
     Vector3 targetPos;
+    Vector3 playerPos;
     GameObject ballObj;
 
-    void Start () {
-        ballObj = GameObject.FindGameObjectWithTag("Ball");
-	}
-	
 	void Update () {
-        targetPos = Vector3.Lerp(gameObject.transform.position,ballObj.transform.position, Time.deltaTime * speed);
-        gameObject.transform.position = new Vector3(20, targetPos.y, 0);
+        ballObj = GameObject.FindGameObjectWithTag("Ball");
+        if (ballObj != null)
+        {
+            targetPos = Vector3.Lerp(gameObject.transform.position, ballObj.transform.position, Time.deltaTime * speed);
+            playerPos = new Vector3(-20, Mathf.Clamp(targetPos.y, -13, 13), 0);
+            gameObject.transform.position = new Vector3(20, playerPos.y, 0);
+        }
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public TextMesh currScore;
+    public GameObject ballPref;
+    public Transform paddleObject;
+
+    GameObject ball;
+    int score;
+
+    void Update() {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        currScore.text = "" + score;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Ball")
+        {
+            score += 1;
+            Destroy(ball);
+            (Instantiate(ballPref, new Vector3(paddleObject.transform.position.x + 2, paddleObject.transform.position.y, 0),Quaternion.identity) as GameObject).transform.parent = paddleObject;
+        }
+    }
+}
