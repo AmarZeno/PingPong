bb6aca0db02dc2a1ebf1930aeb8908cf75208903
Amar Zeno <Amar Zeno>
2016-02-07 23:26:37 +0530

    Game progress with physics addition

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	public float paddleSpeed = 1;
+	public Vector3 playerPos;
+	void Update () {
+		float yPos = gameObject.transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+		playerPos = new Vector3 (-20, Mathf.Clamp(yPos,-13,13),0);
+		gameObject.transform.position = playerPos;
+	}
+}
